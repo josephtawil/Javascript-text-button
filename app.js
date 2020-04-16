da716a838210517dff7c339cad732cf3138a2ece
Joseph Tawil <jtawil2015@gmail.com>
2020-04-15 22:12:58 -0700

    created text input bar with event listeners

---
+++ app.js
@@ -0,0 +1,34 @@
+// var textinput = document.quereySelector("#textInput1");
+// var submitBtn = document.quereySelector("#submitBtn");
+// var textValue;
+// //the arrow is called an arrow function
+// //what is the difference between an arrow function and regular function
+// submitBtn.addEventListener("click", () =>
+// {
+//     textValue = textinput.value;
+//     console.log(textValue);
+//     textinput.value = "";
+// });
+
+
+var textinput = document.querySelector("#textInput");
+var submitBtn = document.querySelector("#submitBtn");
+var container = document.querySelector("#container");
+submitBtn.addEventListener("click", (event) =>
+{
+    event.preventDefault();
+    // console.log(textinput.value);
+
+    // var h1 = document.createElement("h1");
+    // var text = document.createTextNode(textinput.value);
+
+    // h1.appendChild(text);
+
+    // container.appendChild(h1);
+    document.querySelector()
+    {
+        "#container2"
+    }innerHTML = <h1>$[textinput.value]</h1>
+});
+
+// document.querySelector("#container2").innerHTML = "<h1>$[textinput.value]<h1>";
